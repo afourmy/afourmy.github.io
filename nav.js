95afe7e037373c5e55ea65568fb6c96a94b28ce0
Antoine Fourmy <antoine.fourmy@gmail.com>
2026-05-01 03:00:20 +0000

    update

--- nav.js
+++ nav.js
@@ -2,13 +2,13 @@
   var menu = [
     { en: "Mathematics", fr: "Math\u00e9matiques", cssClass: "submenu-sections", columns: [
       [
-        { href: "math/l1/bases.html", en: "Foundations", fr: "Bases" },
-        { href: "math/l1/groups.html", en: "Groups", fr: "Groupes" },
+        { href: "math/bases.html", en: "Foundations", fr: "Bases" },
+        { href: "math/groups.html", en: "Groups", fr: "Groupes" },
       ],
       [
-        { href: "math/l1/probabilites.html", en: "Probability", fr: "Probabilit\u00e9s" },
-        { href: "math/l1/denombrement.html", en: "Combinatorics", fr: "D\u00e9nombrement" },
-        { href: "math/l1/complexes.html", en: "Complex Numbers", fr: "Nombres complexes" },
+        { href: "math/probabilites.html", en: "Probability", fr: "Probabilit\u00e9s" },
+        { href: "math/denombrement.html", en: "Combinatorics", fr: "D\u00e9nombrement" },
+        { href: "math/complexes.html", en: "Complex Numbers", fr: "Nombres complexes" },
       ]
     ]},
     { en: "Computer Science", fr: "Informatique", columns: [
